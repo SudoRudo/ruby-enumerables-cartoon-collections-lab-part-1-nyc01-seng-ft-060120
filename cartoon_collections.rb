dd502976fb2f3584aad9b1dc7cf1586e13dab50e
Rudy Guerrero <r.guerreronyc@gmail.com>
2020-05-14 04:58:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  array.each_with_index do |name, index|
+  array.each_with_index do |name|
     index = array[name]
     p index name
   end
